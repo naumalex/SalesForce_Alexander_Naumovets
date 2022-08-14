e1da19ad51bb79b5e76b0206455eca86678af120
alexn <naum1979alex@gmail.com>
2022-08-15 00:02:33 +0300

    removed commented code

--- src/test/java/pages/models/Account.java
+++ src/test/java/pages/models/Account.java
@@ -24,19 +24,7 @@ public class Account {
     private String description;
     private String fullBillingAddress;
     private String fullShippingAddress;
-    /*private String billingStreet;
-    private String billingCity;
-    private String billingStateProvince;
-    private String billingZipPostalCode;
-    private String billingCountry;
-    private String shippingStreet;
-    private String shippingCity;
-    private String shippingStateProvince;
-    private String shippingZipPostalCode;
-    private String shippingCountry;*/
     private boolean isCopyBillingAddressToShippingAddress;
-   /* private String fullBillingAddress;
-    private String fullShippingAddress;*/
     private Address billingAddress;
     private Address shippingAddress;
 
@@ -54,26 +42,6 @@ public class Account {
         return Optional.ofNullable(billingAddress);
     }
 
-
-/*    public String getFullBillingAddress() {
-        return Optional.ofNullable(fullBillingAddress).orElse(
-                Utils.buildFullAddress(street, city, stateProvince, zipPostalCode, country));
-    }*/
-
-   /* @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Account account = (Account) o;
-        return isCopyBillingAddressToShippingAddress == account.isCopyBillingAddressToShippingAddress && Objects.equals(phone, account.phone) && Objects.equals(fax, account.fax) && Objects.equals(accountName, account.accountName) && Objects.equals(parentAccount, account.parentAccount) && Objects.equals(website, account.website) && accountType == account.accountType && Objects.equals(employees, account.employees) && industry == account.industry && Objects.equals(AnnualRevenue, account.AnnualRevenue)
-                && Objects.equals(description, account.description)
-                && Objects.equals(billingAddress.map(Address::getFullAddress).orElse(""),
-                account.billingAddress.map(Address::getFullAddress).orElse(""))
-                && Objects.equals(shippingAddress.map(Address::getFullAddress).orElse(""),
-                account.shippingAddress.map(Address::getFullAddress).orElse(""));
-    }
-
-    */
 
     @Override
     public boolean equals(Object o) {
