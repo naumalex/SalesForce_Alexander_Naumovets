3da0c6b95580717b5f3a88d42273b5b18b00c48b
alexn <naum1979alex@gmail.com>
2022-08-19 10:36:51 +0300

    Fixed Issue 9. Tidy up HomePage.java

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -8,12 +8,7 @@ import org.openqa.selenium.interactions.Actions;
 @Log4j2
 public class HomePage extends BasePage{
     private final static By USER_ICON_LOCATOR = By.cssSelector(".photoContainer");
-    private final static By SHOW_NAVIGATION_MENU_BUTTON_LOCATOR =
-            By.cssSelector("button[title= 'Show Navigation Menu']");
-    private final static String NAVIGATION_MENU_ITEM_LOCATOR = "a[data-label= '%s']";
-    private final static String CLOSE_TAB_BUTTON_LOCATOR = "button[title= 'Close %s']";
     private final static String LOGOUT_LINK_LOCATOR =
-          //  By.cssSelector("*[class~='userProfilePanel'] a[class~='logout']");
     "//*[contains(@class, 'userProfilePanel')]//a[text() = '%s']";
     public HomePage(WebDriver driver) {
         super(driver);
@@ -25,44 +20,23 @@ public class HomePage extends BasePage{
     }
 
     public void openTabByName(String tabName) {
-        //By tabLocator = By.cssSelector(String.format(NAVIGATION_MENU_ITEM_LOCATOR, tabName));
         By tabLocator = By.xpath(String.format("//span[text()='%s']", tabName));
-        //driver.findElement(SHOW_NAVIGATION_MENU_BUTTON_LOCATOR).click(); //open menu
         waitForElementClickable(tabLocator);
         jsClick(driver.findElement(tabLocator));//select item in the menu
     }
 
-    public void closeTabByName(String tabName) {
-        By locator = By.cssSelector(String.format(CLOSE_TAB_BUTTON_LOCATOR, tabName));
-        waitForElementClickable(locator);
-        driver.findElement(locator).click();
-    }
-
-    public void waitForTabLoaded(String tabName) {
-       // By tabTitleLocator = By.cssSelector(String.format(TAB_TITLE_LOCATOR, tabName));
-       // waitForElementClickable(tabTitleLocator);
-        //driver.findElement(tabTitleLocator).click();
-    }
     public void logout() {
         openUserMenu();
         clickLinkInUserMenu("Log Out");
     }
     private void openUserMenu() {
         log.info(driver.findElements(USER_ICON_LOCATOR).size());
-        //driver.navigate().refresh();
         Actions action = new Actions(driver);
         action.moveToElement(driver.findElement(USER_ICON_LOCATOR)).build().perform();
         waitForElementDisplayed(USER_ICON_LOCATOR);
         waitForElementClickable(USER_ICON_LOCATOR);
-        /*try {
-            Thread.sleep(5000);
-        }
-        catch (InterruptedException exc)
-        {}*/
         log.info("Click Menu");
-
         driver.findElement(USER_ICON_LOCATOR).click();
-        //jsClick(driver.findElement(USER_ICON_LOCATOR));
         log.info("wait");
         waitForElementClickable(By.xpath(String.format(LOGOUT_LINK_LOCATOR,
                 "Add Username")));
@@ -73,5 +47,4 @@ public class HomePage extends BasePage{
         waitForElementClickable(locator);
         driver.findElement(locator).click();
     }
-
 }
